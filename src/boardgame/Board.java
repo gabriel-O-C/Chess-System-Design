a141b06bf27e12bbea0e056921f1870365b58d60
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2021-03-11 16:38:05 -0300

    moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -59,6 +59,20 @@ public class Board {
         return piece(position) != null;
     }
 
+    public Piece removePieces(Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+        if (piece(position) == null){
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+
+        return aux;
+    }
+
 
 
 }
